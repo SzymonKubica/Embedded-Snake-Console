97e7df8d2d4f60c539cd2a5688f72c4d8f98c5ae
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-07 12:32:38 +0200

    Implemented controller and scheduler_task traits for the analog_stick.

--- src/analog_stick.rs
+++ src/analog_stick.rs
@@ -1,26 +1,29 @@
 use arduino_hal::hal::port::{PC0, PC1, PC2};
 use arduino_hal::port::{mode::Analog, Pin};
-use crate::controller::Controller;
+
+use crate::concurrency::SchedulerTask;
+use crate::controller::{Controller, ControllerListener};
 use crate::controller::Direction;
 
-pub struct AnalogStick {
+pub struct AnalogStick<'a> {
     x_pin: Pin<Analog, PC0>,
     y_pin: Pin<Analog, PC1>,
     switch_pin: Pin<Analog, PC2>,
+    listener: &'a dyn ControllerListener,
 }
 
-
-impl AnalogStick {
+impl<'a> AnalogStick<'a> {
     pub fn new(
         x_pin: Pin<Analog, PC0>,
         y_pin: Pin<Analog, PC1>,
-        switch_pin: Pin<Analog, PC2>) -> AnalogStick
-    {
-        AnalogStick { x_pin, y_pin, switch_pin }
+        switch_pin: Pin<Analog, PC2>,
+        listener: &'a dyn ControllerListener) -> AnalogStick {
+
+        AnalogStick { x_pin, y_pin, switch_pin, listener }
     }
 }
 
-impl Controller for AnalogStick {
+impl<'a> Controller for AnalogStick<'a> {
     fn get_direction(&self) -> Direction {
         let peripherals = arduino_hal::Peripherals::take().unwrap();
         let mut adc = arduino_hal::Adc::new(peripherals.ADC, Default::default());
@@ -28,17 +31,24 @@ impl Controller for AnalogStick {
         let y_value: u16 = self.y_pin.analog_read(&mut adc);
 
         if x_value < 200 {
-            return Direction::UP;
+            return Direction::Up;
         }
         if y_value < 200 {
-            return Direction::RIGHT;
+            return Direction::Right;
         }
         if x_value > 800 {
-            return Direction::DOWN;
+            return Direction::Down;
         }
         if y_value > 800 {
-            return Direction::LEFT;
+            return Direction::Left;
         }
-        return Direction::NO_DIRECTION;
+        return Direction::NoDirection;
     }
+}
+
+impl<'a> SchedulerTask for AnalogStick<'a>  {
+    fn run_task(&self, microseconds: i32) -> () {
+
+    }
+
 }
