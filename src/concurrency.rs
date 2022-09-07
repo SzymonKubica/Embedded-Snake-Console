22683464106d59021bc51da6f1f0c88c309ede71
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-07 12:35:09 +0200

    Added a concurrency module which will handle the concurrent polling of the analog stick controller and operation of the game engine.

---
+++ src/concurrency.rs
@@ -0,0 +1,28 @@
+use arrayvec::ArrayVec;
+
+
+pub trait SchedulerTask {
+    fn run_task(&self, microseconds: i32) -> ();
+}
+
+pub struct Scheduler<'a> {
+    tasks: ArrayVec<&'a dyn SchedulerTask, 10>,
+    time_slice: i32
+}
+
+impl<'a> Scheduler<'a> {
+    pub fn new(tasks: ArrayVec<&'a dyn SchedulerTask, 10>, time_slice: i32)
+    -> Scheduler {
+        Scheduler { tasks, time_slice }
+    }
+}
+
+impl<'a> Scheduler<'a> {
+    pub fn run(&self) -> () {
+        loop {
+            for task in self.tasks.iter() {
+                task.run_task(self.time_slice);
+            }
+        }
+    }
+}
