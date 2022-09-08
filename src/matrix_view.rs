6531583b601be892e56e401f5dee3ec92f0d7959
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-08 10:53:49 +0200

    Added the matrix view module to abstract out the functionality of displaying the game.

---
+++ src/matrix_view.rs
@@ -0,0 +1,20 @@
+use crate::mvc::{View, Task};
+
+pub struct GameView {
+}
+
+impl View for GameView {
+    fn display(&self) -> () {
+        todo!()
+    }
+
+    fn update(&mut self) -> () {
+        todo!()
+    }
+}
+
+impl Task for GameView {
+    fn run(&mut self) -> () {
+        todo!()
+    }
+}
