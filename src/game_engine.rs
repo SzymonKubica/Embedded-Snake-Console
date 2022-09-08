3f0c7fdbb750cdb9b77c6347cb57511fbd6bb29c
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-08 10:52:43 +0200

    Update the game engine to implenment Model and Task traits.

--- src/game_engine.rs
+++ src/game_engine.rs
@@ -1,25 +1,30 @@
-use crate::concurrency::SchedulerTask;
-use crate::controller::{Direction, ControllerListener};
+use crate::mvc::{Direction, Model, View, Task};
 
-
-pub struct GameEngine {
+pub struct GameEngine<'a> {
+    view: &'a mut dyn View,
     chosen_direction: Direction
 }
 
-impl GameEngine {
-    pub fn new() -> GameEngine {
-        GameEngine { chosen_direction: Direction::NoDirection }
+impl<'a> GameEngine<'a> {
+    pub fn new(view: &'a mut dyn View) -> GameEngine {
+        GameEngine {
+            view,
+            chosen_direction: Direction::NoDirection }
     }
 }
 
-impl ControllerListener for GameEngine {
+impl<'a> Model for GameEngine<'a> {
     fn on_input(&mut self, input: Direction) {
         self.chosen_direction = input;
     }
+
+    fn set_view(&mut self, view: &mut dyn View) -> () {
+        todo!()
+    }
 }
 
-impl SchedulerTask for GameEngine {
-    fn run_task(&mut self, microseconds: u32) -> () {
-
+impl<'a> Task for GameEngine<'a> {
+    fn run(&mut self) -> () {
+        todo!()
     }
 }
