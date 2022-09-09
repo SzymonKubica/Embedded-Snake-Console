b3f118e7bd4f7feaf08a33a110dcb0b197f47163
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-09 11:46:01 +0200

    Simplified and cleaned up the initialisation code inside of main.

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,28 @@
 #![feature(abi_avr_interrupt)]
 #![feature(trait_upcasting)]
 #![allow(incomplete_features)]
+#![feature(asm_experimental_arch)]
+
+use core::panic::PanicInfo;
 
 extern crate arduino_hal;
 extern crate avr_device;
 extern crate embedded_hal;
 extern crate arrayvec;
 
-mod analog_stick;
 mod mvc;
+mod common;
+mod time_util;
 mod game_engine;
 mod matrix_view;
-mod time_util;
+mod analog_stick;
+#[allow(warnings, unused)]
+mod shift_register;
+mod internal_representation;
 
-use core::panic::PanicInfo;
+use mvc::View;
 
+use crate::mvc::Task;
 use crate::analog_stick::AnalogStick;
 use crate::game_engine::GameEngine;
 use crate::matrix_view::GameView;
@@ -29,53 +37,20 @@ fn panic(_info: &PanicInfo) -> ! {
 #[arduino_hal::entry]
 fn main() -> ! {
     loop {
-        let peripherals = arduino_hal::Peripherals::take().unwrap();
-        let pins = arduino_hal::pins!(peripherals);
 
-
-        let mut clock_pin = pins.d10.into_output();
-        let mut latch_pin = pins.d11.into_output();
-        let mut data_pin = pins.d12.into_output();
-
-        let ground_pins = (
-            pins.d2.into_output_high(),
-            pins.d3.into_output_high(),
-            pins.d4.into_output_high(),
-            pins.d5.into_output_high(),
-            pins.d6.into_output_high(),
-            pins.d7.into_output_high(),
-            pins.d8.into_output_high(),
-            pins.d9.into_output_high(),
-        );
-
-        const DELAY_INTERVAL: i32 = 100;
-        const FRAME_MOVE_THRESHOLD: i32 = 25;
-        let mut frame_counter = 0;
-
-        let peripherals = arduino_hal::Peripherals::take().unwrap();
-        let mut adc = arduino_hal::Adc::new(peripherals.ADC, Default::default());
-
-        let x_pin = pins.a0.into_analog_input(&mut adc);
-        let y_pin = pins.a1.into_analog_input(&mut adc);
-        let switch_pin = pins.a2.into_analog_input(&mut adc);
-
-        let mut x_val: i32 = 0;
-        let mut y_val: i32 = 0;
-        let mut s_val: i32 = 0;
-
-        let mut view: GameView = GameView {  };
-
-
+        let mut view: GameView = GameView::new();
         let mut engine: GameEngine = GameEngine::new(&mut view);
+        let mut stick : AnalogStick = AnalogStick::new(&mut engine);
 
-        // Initialise the analog stick and let the scheduler set it.
-        let mut stick : AnalogStick = AnalogStick::new(
-            x_pin,
-            y_pin,
-            switch_pin,
-            &mut adc,
-            &mut engine);
+        let mut test_game_board = game_engine::initialize_board();
 
+        test_game_board[3][3] = common::SNAKE_SEGMENT;
+
+        view.update(test_game_board);
+
+        loop {
+            view.run();
+        }
     }
 }
 
