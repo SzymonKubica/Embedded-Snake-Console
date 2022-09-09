5c5de6c5dba6ee4ea93cb2255aa4f195db438757
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-09 13:06:50 +0200

    Added new initialisation code which takes all necessary pins from the peripherals and passes them downstream.

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,9 @@ mod analog_stick;
 mod shift_register;
 mod internal_representation;
 
+use matrix_view::GroundPins;
 use mvc::View;
+use shift_register::ShiftRegister;
 
 use crate::mvc::Task;
 use crate::analog_stick::AnalogStick;
@@ -37,16 +39,39 @@ fn panic(_info: &PanicInfo) -> ! {
 #[arduino_hal::entry]
 fn main() -> ! {
     loop {
+        let peripherals = arduino_hal::Peripherals::take().unwrap();
+        let pins = arduino_hal::pins!(peripherals);
 
-        let mut view: GameView = GameView::new();
-        let mut engine: GameEngine = GameEngine::new(&mut view);
-        let mut stick : AnalogStick = AnalogStick::new(&mut engine);
+        // Initialise the view.
+        let clock_pin = pins.d10.into_output();
+        let latch_pin = pins.d11.into_output();
+        let data_pin = pins.d12.into_output();
 
-        let mut test_game_board = game_engine::initialize_board();
+        let ground_pins = GroundPins::new(
+            pins.d2.into_output_high(),
+            pins.d3.into_output_high(),
+            pins.d4.into_output_high(),
+            pins.d5.into_output_high(),
+            pins.d6.into_output_high(),
+            pins.d7.into_output_high(),
+            pins.d8.into_output_high(),
+            pins.d9.into_output_high());
 
-        test_game_board[3][3] = common::SNAKE_SEGMENT;
+        let shift_register = ShiftRegister::new(clock_pin, latch_pin, data_pin);
 
-        view.update(test_game_board);
+        let mut view = GameView::new(shift_register, ground_pins);
+
+        // Initialise the engine.
+        let mut engine = GameEngine::new(&mut view);
+
+        // Initialise the controller.
+        let mut ad_converter = arduino_hal::Adc::new(
+            peripherals.ADC, Default::default());
+        let x_pin = pins.a0.into_analog_input(&mut ad_converter);
+        let y_pin = pins.a1.into_analog_input(&mut ad_converter);
+        let switch_pin = pins.a2.into_analog_input(&mut ad_converter);
+
+        let mut stick = AnalogStick::new(x_pin, y_pin, switch_pin, ad_converter, &mut engine);
 
         loop {
             view.run();
