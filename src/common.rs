344e2b2ca0ec97677c0708ed43fca27f5f2bd691
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-09 11:48:29 +0200

    Moved commonly used functions to common.rs

---
+++ src/common.rs
@@ -0,0 +1,10 @@
+pub const MAX_SNAKE_LENGTH: usize = 64;
+// Abstraction for game board matrix entries to represent snake and apples.
+pub const SNAKE_SEGMENT: u8 = 1;
+pub const APPLE: u8 = 2;
+
+
+// Constants for the screen.
+pub const SCREEN_REFRESH_INTERVAL: u16 = 100;
+pub const SCORE_DISPLAY_TIME: u16 = 1000;
+pub const FRAMES_BETWEEN_STEPS: u16 = 32;
