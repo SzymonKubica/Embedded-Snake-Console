4eb7f3074dba50a6ea58a63c09e96fc3b75d9d20
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-09 08:37:22 +0200

    Added get_opposite function to the Direction enum to be able to filter out requests to change snake's direction by 180 degrees.

--- src/mvc.rs
+++ src/mvc.rs
@@ -40,10 +40,23 @@ impl<'a, T: Controller<'a>> Task for T {
     }
 }
 
+#[derive(Copy, Clone)]
 pub enum Direction {
     Left,
     Right,
     Up,
     Down,
     NoDirection
+}
+
+impl Direction {
+    pub fn get_opposite(direction: Direction) -> Direction {
+        match direction {
+            Direction::Left        => Direction::Right,
+            Direction::Right       => Direction::Left,
+            Direction::Up          => Direction::Down,
+            Direction::Down        => Direction::Up ,
+            Direction::NoDirection => Direction::NoDirection,
+        }
+    }
 }
