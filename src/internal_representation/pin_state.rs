802223670aedd2be9aadfd70b127dfa10f35c46a
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-14 10:45:06 +0200

    Added an abstraction for the pin state (high/low) which replaced the usage of boolean values inside of the analog_stick

---
+++ src/internal_representation/pin_state.rs
@@ -0,0 +1,14 @@
+#[derive(Copy, Clone, PartialEq)]
+pub enum PinState {
+    High, Low
+}
+
+impl From<bool> for PinState {
+    fn from(state: bool) -> Self {
+        if state {
+            Self::High
+        } else {
+            Self::Low
+        }
+    }
+}
