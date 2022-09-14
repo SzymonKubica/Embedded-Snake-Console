23d490b3616f99c0bc106b51aeec157f49a84a0f
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-14 22:10:22 +0200

    Refactored snake struct.

--- src/internal_representation/snake.rs
+++ src/internal_representation/snake.rs
@@ -19,23 +19,18 @@ impl Snake {
         let mut segments: ArrayVec<Point, MAX_SNAKE_LENGTH> = ArrayVec::new();
         segments.push(head);
 
-        Snake {
-            segments,
-            head,
-            direction: Direction::Right
-        }
+        Snake { segments, head, direction: Direction::Right }
     }
 
     pub fn move_head(&mut self) {
-        let new_head: Point = match self.direction {
+        self.head = match self.direction {
             Direction::Left        => self.head.translate_left(),
             Direction::Right       => self.head.translate_right(),
             Direction::Up          => self.head.translate_up(),
             Direction::Down        => self.head.translate_down(),
             Direction::NoDirection => self.head // Unreachable.
         };
-        self.head = new_head;
-        self.add_segment(self.head);
+        self.segments.push(self.head);
     }
 
     pub fn advance_tail(&mut self) -> Point {
@@ -47,19 +42,12 @@ impl Snake {
         // of the current direction. That would cause the snake to crash into
         // itself. We also don't overwrite the direction if it didn't change.
         if self.direction == new_direction ||
-           self.direction == Direction::get_opposite(new_direction) {
-
-           return;
-        }
+           self.direction == Direction::get_opposite(new_direction) { return; }
 
         match new_direction {
             Direction::NoDirection => (), // Snake cannot be stationary.
             _ => self.direction = new_direction
         };
-    }
-
-    fn add_segment(&mut self, segment: Point) {
-        self.segments.push(segment)
     }
 }
 
