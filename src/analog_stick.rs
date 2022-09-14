e26b1de2e4f39c3d1abdac373b91deefa0932aaa
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-14 10:39:59 +0200

    Updated the analog_stick to correctly register toggling the switch and filter out prolonged presses.

--- src/analog_stick.rs
+++ src/analog_stick.rs
@@ -3,7 +3,8 @@ use arduino_hal::port::mode::{Input, PullUp};
 use arduino_hal::hal::port::{PC0, PC1, PC2};
 use arduino_hal::port::{mode::Analog, Pin};
 
-use crate::mvc::{Model, Controller, Direction, ControllerInput};
+use crate::internal_representation::pin_state::PinState;
+use crate::mvc::{Controller, Direction, ControllerInput};
 
 const ANALOG_LOWER_THRESHOLD: u16 = 200;
 const ANALOG_UPPER_THRESHOLD: u16 = 800;
@@ -12,6 +13,7 @@ pub struct AnalogStick {
     x_pin: Pin<Analog, PC0>,
     y_pin: Pin<Analog, PC1>,
     switch_pin: Pin<Input<PullUp>, PC2>,
+    switch_state: PinState,
     ad_converter: Adc,
 }
 
@@ -26,6 +28,7 @@ impl<'a> AnalogStick {
             x_pin,
             y_pin,
             switch_pin,
+            switch_state: PinState::High,
             ad_converter }
     }
 }
@@ -45,24 +48,38 @@ impl Controller for AnalogStick {
     fn read_input(&mut self) -> ControllerInput {
         let x_value: u16 = self.x_pin.analog_read(&mut self.ad_converter);
         let y_value: u16 = self.y_pin.analog_read(&mut self.ad_converter);
+        let switch_input = PinState::from(self.switch_pin.is_high());
 
-        let mut direction: Direction = Direction::NoDirection;
+        let toggle_signal = toggle_registered(switch_input, self.switch_state);
+        let direction = determine_direction(x_value, y_value);
 
-        if x_value < ANALOG_LOWER_THRESHOLD {
-             direction = Direction::Up;
-        }
-        if x_value > ANALOG_UPPER_THRESHOLD {
-            direction = Direction::Down;
-        }
-        if y_value < ANALOG_LOWER_THRESHOLD {
-            direction = Direction::Right;
-        }
-        if y_value > ANALOG_UPPER_THRESHOLD {
-            direction = Direction::Left;
-        }
+        self.switch_state = switch_input;
 
-        let switch_input: bool = self.switch_pin.is_low();
-
-        ControllerInput::new(switch_input, direction)
+        ControllerInput::new(toggle_signal, direction)
     }
 }
+
+fn toggle_registered(new_state: PinState, old_state: PinState) -> bool {
+    old_state == PinState::High && new_state == PinState::Low
+}
+
+fn determine_direction(x_value: u16, y_value: u16) -> Direction {
+    let mut direction: Direction = Direction::NoDirection;
+
+    if x_value < ANALOG_LOWER_THRESHOLD {
+         direction = Direction::Up;
+    }
+    if x_value > ANALOG_UPPER_THRESHOLD {
+        direction = Direction::Down;
+    }
+    if y_value < ANALOG_LOWER_THRESHOLD {
+        direction = Direction::Right;
+    }
+    if y_value > ANALOG_UPPER_THRESHOLD {
+        direction = Direction::Left;
+    }
+
+    direction
+}
+
+
