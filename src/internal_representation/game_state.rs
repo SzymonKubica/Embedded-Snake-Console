a8af7bbf83b3c7cb0b7b67b34b854173ec52c899
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-14 21:18:32 +0200

    Added functions to be able to check if it is the time for the snake to move forward and to register the move at a particular instant.

--- src/internal_representation/game_state.rs
+++ src/internal_representation/game_state.rs
@@ -1,4 +1,5 @@
 use crate::libs::time_util::millis;
+use crate::common::SNAKE_MOVE_INTERVAL;
 
 pub struct GameState {
     pub score: u8,
@@ -8,7 +9,14 @@ pub struct GameState {
 
 impl GameState {
     pub fn new() -> GameState {
-
         GameState { score: 0, last_move_timestamp: millis(), is_active: false }
+    }
+
+    pub fn is_time_for_next_move(&self) -> bool {
+        millis() - self.last_move_timestamp >= SNAKE_MOVE_INTERVAL
+    }
+
+    pub fn register_move_at(&mut self, time: u32) {
+        self.last_move_timestamp = time;
     }
 }
