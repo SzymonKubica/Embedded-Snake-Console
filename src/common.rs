507a08f017acd3b29af6fc4094bd0c6ae282448c
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-15 21:55:55 +0200

    Updated the analog stick polling interval in the common.rs

--- src/common.rs
+++ src/common.rs
@@ -1,4 +1,5 @@
 pub const BOARD_SIZE: usize = 8;
+pub const MAX_SCORE: usize = BOARD_SIZE * BOARD_SIZE;
 
 // The time between two steps of the snake,
 // effectively controlling the speed of the game. (miliseconds)
@@ -6,4 +7,4 @@ pub const SNAKE_MOVE_INTERVAL: u32 = 250;
 
 // The time between calls to controller.get_input(). Controlls the
 // responsiveness of the controlls. (miliseconds)
-pub const CONTROLLER_POLLING_INTERVAL: u32 = 100;
+pub const CONTROLLER_POLLING_INTERVAL: u32 = 50;
