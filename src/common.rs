a645615b093a6529faa6cb007177f2c3109b1eeb
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-16 21:35:19 +0200

    Added constants for different velocities of the snake.

--- src/common.rs
+++ src/common.rs
@@ -1,10 +1,12 @@
 pub const BOARD_SIZE: usize = 8;
-pub const MAX_SCORE: usize = BOARD_SIZE * BOARD_SIZE;
+pub const MAX_SCORE: usize = BOARD_SIZE * BOARD_SIZE - 1;
 
 // The time between two steps of the snake,
 // effectively controlling the speed of the game. (miliseconds)
-pub const SNAKE_MOVE_INTERVAL: u32 = 250;
+pub const SNAKE_MOVE_INTERVAL_FAST: u32 = 125;
+pub const SNAKE_MOVE_INTERVAL_NORMAL: u32 = 250;
+pub const SNAKE_MOVE_INTERVAL_SLOW: u32 = 500;
 
 // The time between calls to controller.get_input(). Controlls the
 // responsiveness of the controlls. (miliseconds)
-pub const CONTROLLER_POLLING_INTERVAL: u32 = 50;
+pub const CONTROLLER_POLLING_INTERVAL: u32 = 100;
