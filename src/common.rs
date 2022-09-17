52dc007cb351d23fe5ed2a745b24eebdae782df3
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-18 00:12:27 +0200

    Adjusted the three game speeds to make the default one slower and the slow one faster.

--- src/common.rs
+++ src/common.rs
@@ -3,10 +3,10 @@ pub const MAX_SCORE: usize = BOARD_SIZE * BOARD_SIZE - 1;
 
 // The time between two steps of the snake,
 // effectively controlling the speed of the game. (miliseconds)
-pub const SNAKE_MOVE_INTERVAL_FAST: u32 = 125;
-pub const SNAKE_MOVE_INTERVAL_NORMAL: u32 = 250;
-pub const SNAKE_MOVE_INTERVAL_SLOW: u32 = 500;
+pub const SNAKE_MOVE_INTERVAL_FAST: u32 = 200;
+pub const SNAKE_MOVE_INTERVAL_NORMAL: u32 = 300;
+pub const SNAKE_MOVE_INTERVAL_SLOW: u32 = 400;
 
 // The time between calls to controller.get_input(). Controlls the
 // responsiveness of the controlls. (miliseconds)
-pub const CONTROLLER_POLLING_INTERVAL: u32 = 100;
+pub const CONTROLLER_POLLING_INTERVAL: u32 = 33;
